8291a8c5760c491e24643b870f6c9d6152c0b9a5
Ogunniyi07 <ogunniyi.julius12@gmail.com>
2022-06-16 21:35:40 +0100

    Updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import './About.css';
 
 const About = () => {
+
+
   return (
     <div className="about">
         <div className="about-hero">
@@ -25,35 +27,13 @@ const About = () => {
           <div className="about-blur"></div>
           <div className="about-goal">
             <div className="goal">
-              <p>Our Goal</p>
+              <p>Our goal</p>
             </div>
             <div className="card">
               <p>ISE came about as a means to help domestic workers looking to reach a larger audience and also to help people too occupied to do little jobs around. Our expertise grow with each year, and our accumulated experience empowers us to develop well enough to serve our clients.</p>
             </div>
           </div>
         </div>
-      {/* <div className="about-section">
-        <div className="about-mobile">
-          <img src="\images\mobile.png" alt="" />
-        </div>
-        <div className="about-mobiletext">
-          <h2>With a One way App where you can be either a worker or an employer or even both!!</h2>
-        </div>
-      </div> */}
-      {/* <div className="about-numbers">
-        <div className="about-customers">
-          <h2>3000+</h2>
-          <p>Customers</p>
-        </div>
-        <div className="about-customers">
-          <h2>2500+</h2>
-          <p>Happy Customers</p>
-        </div>
-        <div className="about-customers">
-          <h2>30</h2>
-          <p>Staffs</p>
-        </div>
-      </div> */}
       <div className="about-sec1">
         <div className="about-blur1"></div>
         <div className="about-rect">
@@ -67,7 +47,63 @@ const About = () => {
       </div>
       <div className="about-sponsor">
         <h2>Our Sponsors</h2>
-        <marquee behavior="scroll" direction="left" scrollamount="5">
+        <div id="maindiv">
+  <div id="div1">
+    &nbsp;Test-1 Test-2 Test-3 Test-4 Test-5 Test-6 Test-7 Test-8 Test-9 Test-10 Test-11
+  </div>
+  <div id="div2">
+    &nbsp;Test-1 Test-2 Test-3 Test-4 Test-5 Test-6 Test-7 Test-8 Test-9 Test-10 Test-11
+  </div>
+</div>
+        {/* <div className="marquee">
+          <div className="slide1">
+          <div className="sponsor-card">
+          <img src="\images\sponsor4.png" alt="" />
+          </div>
+          <div className="sponsor-card">
+            <img src="\images\sponsor2.png" alt="" />
+          </div>
+          <div className="sponsor-card">
+            <img src="\images\sponsor1.png" alt="" />
+          </div>
+          <div className="sponsor-card">
+            <img src="\images\sponsor3.png" alt="" />
+          </div>  
+          <div className="sponsor-card">
+            <img src="\images\sponsor5.png" alt="" />
+          </div> 
+          <div className="sponsor-card">
+            <img src="\images\sponsor6.png" alt="" />
+          </div> 
+          <div className="sponsor-card">
+            <img src="\images\sponsor7.png" alt="" />
+          </div>        
+          </div>
+          <div className="slide2">
+          <div className="sponsor-card">
+          <img src="\images\sponsor4.png" alt="" />
+          </div>
+          <div className="sponsor-card">
+            <img src="\images\sponsor2.png" alt="" />
+          </div>
+          <div className="sponsor-card">
+            <img src="\images\sponsor1.png" alt="" />
+          </div>
+          <div className="sponsor-card">
+            <img src="\images\sponsor3.png" alt="" />
+          </div>  
+          <div className="sponsor-card">
+            <img src="\images\sponsor5.png" alt="" />
+          </div> 
+          <div className="sponsor-card">
+            <img src="\images\sponsor6.png" alt="" />
+          </div> 
+          <div className="sponsor-card">
+            <img src="\images\sponsor7.png" alt="" />
+          </div>        
+          </div>
+        </div> */}
+        {/* <marquee behavior="scroll" direction="left" scrollamount="5">
         <div className="sponsor-card-row">
           <div className="sponsor-card">
           <img src="\images\sponsor4.png" alt="" />
@@ -91,7 +127,7 @@ const About = () => {
             <img src="\images\sponsor7.png" alt="" />
           </div>        
         </div>
-        </marquee>
+        </marquee> */}
       </div>
       <div className="about-btn">
         <button type="button">Become a Partner</button>
